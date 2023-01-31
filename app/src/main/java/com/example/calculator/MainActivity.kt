b2b596c45e2a8d2c04fef560c3f365265b0d2817
KhaledaAlRusan <khaled66658032@gmail.com>
2023-01-31 01:44:33 -0500

    second commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,62 +5,58 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.calculator.databinding.ActivityMainBinding
+
 //This calculator has problems
 //I can't do multiple operation at one time, I need to press equal every single time
 //I can press Dot two times after each other
 class MainActivity : AppCompatActivity() {
-    lateinit var textView: TextView
-    lateinit var buttonPlus: Button
-    lateinit var buttonMinus: Button
-    lateinit var buttonDiv: Button
-    lateinit var buttonResult: Button
-    lateinit var buttonMulti: Button
-    lateinit var buttonClear: Button
-    lateinit var buttonPercentage : Button
-    lateinit var buttonSignChange:Button
-    lateinit var buttonDot:Button
     var lastNumber:Double = 0.0
     var currentOperation:Operation? = null
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initView()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         addCallBacks()
     }
 
     private fun addCallBacks() {
-        buttonClear.setOnClickListener{
+
+        binding.btnC.setOnClickListener{
             clearInput()
         }
-        buttonPlus.setOnClickListener{
+
+        binding.btnPlus.setOnClickListener{
             prepareOperation(Operation.Plus)
 
         }
-        buttonMinus.setOnClickListener{
+        binding.btnMinus.setOnClickListener{
             prepareOperation(Operation.Minus)
         }
-        buttonMulti.setOnClickListener{
+        binding.btnMulti.setOnClickListener{
             prepareOperation(Operation.Mul)
         }
-        buttonDiv.setOnClickListener{
+        binding.btnDivide.setOnClickListener{
             prepareOperation(Operation.Div)
         }
-        buttonPercentage.setOnClickListener{
+        binding.btnPercentage.setOnClickListener{
             prepareOperation(Operation.Percen)
         }
-        buttonResult.setOnClickListener{
+        binding.btnEqual.setOnClickListener{
             val result = doCurrentOperation()
-            textView.text = result.toString()
+            binding.tvNumber.text = result.toString()
         }
-        buttonSignChange.setOnClickListener{
-            lastNumber = textView.text.toString().toDouble()*(-1)
-            textView.text = lastNumber.toString()
+        binding.btnPlusMinus.setOnClickListener{
+            lastNumber = binding.tvNumber.text.toString().toDouble()*(-1)
+            binding.tvNumber.text = lastNumber.toString()
         }
-
     }
 
     private fun doCurrentOperation():Double {
-        val secondNumber = textView.text.toString().toDouble()
+        val secondNumber = binding.tvNumber.text.toString().toDouble()
         return when(currentOperation){
             Operation.Plus -> lastNumber+secondNumber
             Operation.Minus -> lastNumber-secondNumber
@@ -71,34 +67,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initView() {
-        buttonSignChange = findViewById(R.id.btn_plusMinus)
-        buttonDot = findViewById(R.id.btn_Dot)
-        buttonPlus = findViewById(R.id.btn_plus)
-        buttonDiv = findViewById(R.id.btn_divide)
-        buttonMinus = findViewById(R.id.btn_minus)
-        buttonMulti = findViewById(R.id.btn_multi)
-        buttonResult = findViewById(R.id.btn_equal)
-        textView = findViewById(R.id.tv_number)
-
-        buttonClear = findViewById(R.id.btn_c)
-        buttonPercentage = findViewById(R.id.btn_percentage)
-    }
     private fun clearInput(){
-        textView.text = ""
+        binding.tvNumber.text = ""
     }
+
     fun onClickNumber(v:View){
-        val oldDigit = textView.text.toString()
-
-       //buttonDot.isEnabled = !oldDigit.contains('.')
+        val oldDigit = binding.tvNumber.text.toString()
        val newDigit = (v as Button).text.toString()
-        buttonDot.isEnabled = !oldDigit.contains('.')
+        binding.btnDot.isEnabled = !oldDigit.contains('.')
         val newTextNumber = oldDigit + newDigit
-        textView.text = newTextNumber
-
+        binding.tvNumber.text = newTextNumber
     }
+
     private fun prepareOperation(operation: Operation){
-        lastNumber = textView.text.toString().toDouble()
+        lastNumber = binding.tvNumber.text.toString().toDouble()
         clearInput()
         currentOperation = operation
     }
